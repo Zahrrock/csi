c4a512bc7b2f4d157a6bd030f97a61f739aa11dc
Yohann Reynaud <yohann.reynaud@etu.minesparis.psl.eu>
2026-02-04 11:22:26 +0100

    définition des structs

--- include/const.h
+++ include/const.h
@@ -1,6 +1,15 @@
 #ifndef CONST_H
 #define CONST_H
 
+#include <stdbool.h>
+
+// Nombre d'entités max autorisé pendant le jeu
+#define NB_BULLET_PLAYER_MAX 10
+#define NB_BULLET_ENEMIES_MAX 10
+#define NB_ENEMY_BY_ROW 6
+#define NB_ENEMY_BY_COLUMN 3
+#define NB_ENEMY NB_ENEMY_BY_ROW*NB_ENEMY_BY_COLUMN
+#define NB_ENTITY_MAX NB_BULLET_PLAYER_MAX+NB_BULLET_ENEMIES_MAX+NB_ENEMY+1
 
 
 
--- include/entity.h
+++ include/entity.h
@@ -1,6 +1,29 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include "const.h"
 
+enum Type{
+    PLAYER = 0,
+    ENEMY_WEAK = 1,
+    ENEMY_STRONG = 2,
+    BULLET_PLAYER = 3,
+    BULLET_ENEMY = 4,
+};
+
+typedef struct {
+    enum Type type;
+    int w, h;
+    float x, y, vx, vy, health;
+    
+} Entity;
+
+typedef struct {
+    bool running;
+    int nb_enemies, nb_bullets_player, nb_bullets_enemy, nb_entities;
+    Entity *player;
+    Entity* Enemies[NB_ENEMY], Bullets_player[NB_BULLET_PLAYER_MAX], Bullets_enemies[NB_BULLET_ENEMIES_MAX], Entity[NB_ENTITY_MAX];
+
+} GAME;
 
 #endif
